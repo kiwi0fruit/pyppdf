a802c87edac629398be241d828067cba3a720cfd
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-01-18 03:11:40 +0700

    commit

--- pyppdf/pyppeteer_pdf.py
+++ pyppdf/pyppeteer_pdf.py
@@ -20,7 +20,7 @@ async def main(args: dict, url: str, output_file: str):
     launch_ = args_kwargs(get('launch', args, {}))
     goto = args_kwargs(get('goto', args, {}))
     goto[1].pop('url', None)
-    emulateMedia = args_kwargs(get('emulateMedia', args))
+    emulate_media = args_kwargs(get('emulateMedia', args))
     waitfor = args_kwargs(get('waitFor', args))
     pdf = args_kwargs(get('pdf', args, {}))
     pdf[1].pop('path', None)
@@ -28,13 +28,13 @@ async def main(args: dict, url: str, output_file: str):
     browser = await launch(*launch_[0], **launch_[1])
     page = await browser.newPage()
 
-    await page.goto(url=url, *goto[0], **goto[1])
+    await page.goto(url, *goto[0], **goto[1])
 
-    if emulateMedia[0] is not None:
-        await page.emulateMedia(*emulateMedia[0], **emulateMedia[1])
+    if emulate_media[0] is not None:
+        await page.emulateMedia(*emulate_media[0], **emulate_media[1])
     if waitfor[0] is not None:
         await page.waitFor(*waitfor[0], **waitfor[1])
-    await page.pdf(path=output_file, *pdf[0], **pdf[1])
+    await page.pdf(path=output_file, **pdf[1])
 
     await browser.close()
 
@@ -61,12 +61,10 @@ def save_pdf(out: str='untitled.pdf', site: str=None, src: str=None,
     Converts html document to pdf via pyppeteer
     and writes to disk.
 
-    ``args_dict`` formats for ``*args`` and ``**kwargs`` for functions:
-        * ``{(): (arg1, arg2), kwarg1=val1, kwarg2=val2}``
-        * ``[arg1, arg2]`` or ``(arg1, arg2)``
-        * ``{kwarg1=val1, kwarg2=val2}``
-        * ``{foo=None}`` means that ``'foo'`` key is not used
-          (same as if it was absent).
+    ``args_dict`` affect the following methods
+    (only the last name should be used): ``pyppeteer.launch``, ``page.goto``,
+    ``page.emulateMedia``, ``page.waitFor``, ``page.pdf``. See:
+     https://miyakogi.github.io/pyppeteer/reference.html#pyppeteer.page.Page.pdf
 
     ``args_dict`` default value:
 
@@ -75,9 +73,18 @@ def save_pdf(out: str='untitled.pdf', site: str=None, src: str=None,
     >>> #       margin={top='1in', right='1in',
     >>> #               bottom='1in', left='1in'},}}
     >>> #
-    ``args_upd`` example that won't overwrite another options:
+
+    ``args_upd`` example that won't overwrite other options:
 
     ``"{launch={args=['--no-sandbox', '--disable-setuid-sandbox']}, emulateMedia="screen", waitFor=1000}"``
+
+    ``args_dict`` formats for ``*args`` and ``**kwargs`` for functions:
+
+    * ``{(): (arg1, arg2), kwarg1=val1, kwarg2=val2}``
+    * ``[arg1, arg2]`` or ``(arg1, arg2)``
+    * ``{kwarg1=val1, kwarg2=val2}``
+    * ``{foo=None}`` means that ``'foo'`` key is not used
+      (same as if it was absent).
 
     Parameters
     ----------
@@ -90,6 +97,7 @@ def save_pdf(out: str='untitled.pdf', site: str=None, src: str=None,
         html document file source
         (only one from site or src must be defined)
     args_dict :
+        Options that govern conversion.
         dict with pyppeteer kwargs or Python code str that would
         be "litereval" evaluated to the dictionary.
         If None then default values are used.
@@ -150,14 +158,19 @@ pyppeteer and writes to disk.
 -d, --dict defaults:
 
 {re.sub(r'^ +', '', ARGS_DICT, flags=re.MULTILINE)}
+
+They affect the following pyppeteer methods (only the last name should be used):
+pyppeteer.launch, page.goto, page.emulateMedia, page.waitFor, page.pdf.
+See: https://miyakogi.github.io/pyppeteer/reference.html#pyppeteer.page.Page.pdf
 """)
 @click.argument('site', type=str, default=None, required=False)
 @click.option('-d', '--dict', 'args_dict', type=str, default=None,
               help='Python code str that would be evaluated to the dictionary that is a ' +
                    'pyppeteer functions options. Has predefined defaults.')
 @click.option('-u', '--upd', 'args_upd', type=str, default=None,
-              help="Same as --dict but this dict is recursively merged into --dict.")
-@click.option('-o', '--out', type=str, default='untitled.pdf', help='Output file path.')
+              help="Same as --dict but --upd dict is recursively merged into --dict.")
+@click.option('-o', '--out', type=str, default='untitled.pdf',
+              help='Output file path. Default is "untitled.pdf"')
 def cli(site, args_dict, args_upd, out):
     kwargs = dict(site=site) if site else dict(src=sys.stdin.read())
     save_pdf(out=out, args_dict=args_dict, args_upd=args_upd, **kwargs)
