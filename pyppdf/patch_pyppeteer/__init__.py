c443ae8226a07f1043d7ca9e65a8db72776f5221
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-12-08 01:23:34 +0700

    Update __init__.py

--- pyppdf/patch_pyppeteer/__init__.py
+++ pyppdf/patch_pyppeteer/__init__.py
@@ -1,4 +1,11 @@
 import os
-os.environ.setdefault('PYPPETEER_CHROMIUM_REVISION', '693954')
+# Check the latest stable Chromium revision here: https://chromium.woolyss.com/
+# But there is some OS specific stuff:
+# https://github.com/Bugazelle/chromium-all-old-stable-versions/blob/master/chromium.stable.csv
+if os.name == 'nt':
+  rev = '693951'
+else:
+  rev = '693954'
+os.environ.setdefault('PYPPETEER_CHROMIUM_REVISION', rev)
 from .patch_pyppeteer import patch_pyppeteer
 patch_pyppeteer()
