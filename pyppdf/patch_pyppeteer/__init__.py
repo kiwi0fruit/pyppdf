fdf8470706695191db608f04fb3f165fcf800787
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-12-07 23:53:10 +0700

    0.0.11

--- pyppdf/patch_pyppeteer/__init__.py
+++ pyppdf/patch_pyppeteer/__init__.py
@@ -1,4 +1,4 @@
 import os
-os.environ.setdefault('PYPPETEER_CHROMIUM_REVISION', '609904')
+os.environ.setdefault('PYPPETEER_CHROMIUM_REVISION', '693954')
 from .patch_pyppeteer import patch_pyppeteer
 patch_pyppeteer()
