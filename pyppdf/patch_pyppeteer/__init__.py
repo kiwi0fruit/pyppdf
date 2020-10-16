f1e9302d24a26aa6c9230c6dc22e75db3c908e22
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2020-10-17 01:17:22 +0700

    Update __init__.py

--- pyppdf/patch_pyppeteer/__init__.py
+++ pyppdf/patch_pyppeteer/__init__.py
@@ -3,7 +3,7 @@ import platform
 # Check the latest stable Chromium revision here: https://chromium.woolyss.com/
 # Then find out the real revision for download (OS specific):
 # https://github.com/Bugazelle/chromium-all-old-stable-versions/blob/master/chromium.stable.csv
-rev = dict(Windows='800229', Linux='800217', Darwin='800208').get(platform.system(), '800218')
+rev = dict(Windows='800229', Linux='782790', Darwin='800208').get(platform.system(), '800218')  # Main is 800218; Linux='800217'
 os.environ.setdefault('PYPPETEER_CHROMIUM_REVISION', rev)
 from .patch_pyppeteer import patch_pyppeteer
 patch_pyppeteer()
